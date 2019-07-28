2a1a770c44b4e46e944aa92fc70550c09474cd13
jade@code-beta.com <jade@code-beta.com>
2019-07-28 12:34:26 +0200

    update config.js details

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'Software Development Blog', // Required
+  author: 'Jade Rickerts', // Required
+  description: 'Business Analyst | .NET Developer',
+  primaryColor: '#00a86b', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://jaderickerts.com',
+    github: 'https://github.com/Lakendary',
+    twitter: 'https://twitter.com/lkn_ant',
+    linkedin: 'https://www.linkedin.com/in/jade-rickerts-44597032/',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/blog',
+  siteUrl: 'https://blog.jaderickerts.com',
 };
